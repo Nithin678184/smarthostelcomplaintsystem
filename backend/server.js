ca1165c80ccecfd7dc111fced86c42e7bd2e640a
Nithin678184 <nithinrblr8703@gmail.com>
2026-02-26 16:04:56 +0530

    Added root route to fix Cannot GET / error

--- backend/server.js
+++ backend/server.js
@@ -28,3 +28,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Smart Hostel Complaint System Backend is Running 🚀");
+});
